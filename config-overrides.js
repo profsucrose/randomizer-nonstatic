6d451fef3f4c7f3b539643b5606cf156020beffe
zane-programs <zstjohn22@windwardschool.org>
2022-02-10 17:44:55 -0800

    2.0.3 - store repo url in .env

--- config-overrides.js
+++ config-overrides.js
@@ -2,21 +2,24 @@ const { execSync } = require("child_process");
 const { DefinePlugin } = require("webpack");
 const parseCSVSync = require("./parse_csv");
 
+// get env variables from .env
+require("dotenv").config();
+
 module.exports = function override(config) {
   console.log(getCommandOutputSync("git remote -v"));
 
   // parse word list csv (synchronous)
   const parsedLists = parseCSVSync("./lists/lists.csv");
 
+  // get current commit hash
   const fullCommitHash = getCommandOutputSync("git rev-parse HEAD");
-  const repoUrl = getCommandOutputSync("git remote get-url origin");
 
   // add my extras
   if (!config.plugins) config.plugins = [];
   config.plugins.push(
     new DefinePlugin({
       __COMMIT_HASH__: fullCommitHash,
-      __REPOSITORY_URL__: repoUrl,
+      __REPOSITORY_URL__: JSON.stringify(process.env.REPOSITORY_URL),
       __RANDOMIZER_LISTS__: JSON.stringify(parsedLists),
     })
   );
